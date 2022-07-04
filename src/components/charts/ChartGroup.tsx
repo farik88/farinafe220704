e555d882c2d839b946f399d50373000cd0ab187a
Farina Alexey <farik88@ua.fm>
2022-07-04 16:16:27 +0300

    ChartGroup make auto view mode switch

--- src/components/charts/ChartGroup.tsx
+++ src/components/charts/ChartGroup.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useEffect, useMemo, useState} from 'react';
+import React, {FC, useEffect, useRef, useState} from 'react';
 import {chartGroupModeType, IChartItem} from "../../types/charts";
 import cl from "../../styles/components/ChartGroup.module.scss"
 import ChartItem from "./ChartItem";
@@ -28,6 +28,27 @@ const getPassedTime = (chartItems: IChartItem[], currentIndex: number) => {
 const ChartGroup:FC<ChartGroupProps> = ({data}) => {
   const [viewMode, setViewMode] = useState<chartGroupModeType>('horizontal')
   const [totalTime, setTotalTime] = useState<number>(0)
+  const rootNodeRef = React.useRef() as React.MutableRefObject<HTMLInputElement>;
+  const innerNodeRef = React.useRef() as React.MutableRefObject<HTMLInputElement>;
+  const [viewModeBreakPoint, setViewModeBreakPoint] = useState<null|number>(null)
+
+  useEffect(() => {
+    // Calc is need to switch view mode
+    const resizeWatcher = setInterval(() => {
+      if (viewMode === "horizontal" && innerNodeRef.current.scrollWidth > rootNodeRef.current.scrollWidth) {
+        setViewModeBreakPoint(window.innerWidth)
+        setViewMode("vertical")
+      }
+
+      if (viewMode === "vertical" && viewModeBreakPoint !== null && viewModeBreakPoint < window.innerWidth) {
+        setViewMode("horizontal")
+      }
+    }, 200)
+
+    return () => {
+      clearInterval(resizeWatcher)
+    }
+  })
 
   useEffect(() => {
     // Calc total time
@@ -37,8 +58,8 @@ const ChartGroup:FC<ChartGroupProps> = ({data}) => {
   }, [data])
 
   return (
-    <div className={cl.group}>
-      <div className={getInnerClasses(viewMode).join(' ')}>
+    <div className={cl.group} ref={rootNodeRef}>
+      <div className={getInnerClasses(viewMode).join(' ')} ref={innerNodeRef}>
         {data.map((chartItem, index) =>
           <ChartItem key={chartItem.name} data={chartItem} startFrom={getPassedTime(data, index)} totalTime={totalTime} viewMode={viewMode}/>
         )}
